e456ee205e8fb634e6bef6756151fabe99ea10f6
cmr927 <142535038+cmr927@users.noreply.github.com>
2024-02-21 21:12:16 -0500

    Created login and signup

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,13 @@ import { MainView } from "./components/main-view/main-view";
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
+const App = () => {
+    return <MainView />;
+};
+
 // Finds the root of your app
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
-root.render(<MainView />);
+root.render(<App />);
